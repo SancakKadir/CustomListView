b94f5b4a14e062224f4bf2eaed40952358c065ab
kadirsancak <kadir.sancak@leanscale.com>
2022-03-20 22:00:05 +0300

    Setup assignment list adapter

--- app/src/main/java/com/example/customlistview/MainActivity.kt
+++ app/src/main/java/com/example/customlistview/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.customlistview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.assignmentlistview.ui.assignmentlistview.AssignmentList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = findViewById<AssignmentList>(R.id.assignment)
+
+        list.setup(
+            listOf(
+                AssignmentList.ItemEntity("https://db62cod6cnasq.cloudfront.net/user-media/0/image1-500kb.png"),
+                AssignmentList.ItemEntity("https://db62cod6cnasq.cloudfront.net/user-media/0/image1-1mb.png"),
+                AssignmentList.ItemEntity("https://db62cod6cnasq.cloudfront.net/user-media/0/image1-1_5mb.png"),
+                AssignmentList.ItemEntity("https://db62cod6cnasq.cloudfront.net/user-media/0/image1-2mb.png"),
+                AssignmentList.ItemEntity("https://db62cod6cnasq.cloudfront.net/user-media/0/image1-3mb.png"),
+                AssignmentList.ItemEntity("https://db62cod6cnasq.cloudfront.net/user-media/0/image1-5mb.png"),
+            )
+        )
     }
 }
--- assignmentlistview/src/main/java/com/example/assignmentlistview/ui/assignmentlistview/AssignmentListAdapter.kt
+++ assignmentlistview/src/main/java/com/example/assignmentlistview/ui/assignmentlistview/AssignmentListAdapter.kt
@@ -1,14 +1,44 @@
 package com.example.assignmentlistview.ui.assignmentlistview
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.example.assignmentlistview.R
 import com.example.assignmentlistview.core.adapter.DefaultItemAdapter
 
 
 class AssignmentListAdapter : DefaultItemAdapter<AssignmentList.ItemEntity>() {
 
-    override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val itemView: View
+        val viewHolder: ItemViewHolder
+        val item = items[position]
 
-        return view
+        if (convertView == null) {
+            itemView = LayoutInflater.from(parent?.context)
+                .inflate(R.layout.item_component_assignment_list, parent, false)
+
+            viewHolder = ItemViewHolder()
+            viewHolder.imageView = itemView.findViewById<View>(R.id.ivAssignment) as ImageView
+
+            itemView.tag = viewHolder
+        } else {
+            itemView = convertView
+            viewHolder = convertView.tag as ItemViewHolder
+        }
+
+        Glide.with(viewHolder.imageView.context)
+            .load(item.imageUrl)
+            .placeholder(R.drawable.ic_place_holder)
+            .into(viewHolder.imageView)
+
+
+        return itemView
+    }
+
+    private class ItemViewHolder {
+        lateinit var imageView: ImageView
     }
 }
